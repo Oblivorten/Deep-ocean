c4d10a508870eb8475dbdb16165eeaf455dc89a0
oblivorten <oblivorten@gmail.com>
2026-07-17 00:45:39 +0300

    Добавил мины и механику их движения

---
+++ Assets/Scripts/MineMovement.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class MineMovement : MonoBehaviour
+{
+   
+    private float speed = 5f;
+    private Vector3 direction = Vector3.left;
+    private float deleteCoordinate = -10f;
+
+    private void Update()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+
+        if (transform.position.x <= deleteCoordinate)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
