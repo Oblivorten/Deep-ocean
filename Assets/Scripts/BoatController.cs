b3ce6e285be084228e8cc638c140f258df384a19
oblivorten <oblivorten@gmail.com>
2026-07-17 01:44:34 +0300

    Добавил механику получения урона

--- Assets/Scripts/BoatController.cs
+++ Assets/Scripts/BoatController.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class BoatController : MonoBehaviour
@@ -5,8 +6,15 @@ public class BoatController : MonoBehaviour
     private int lifes = 3;
     private float speed = 5f;
     private int score = 0;
-    
-    void Update()
+    [SerializeField] private TextMeshProUGUI hpText;
+    [SerializeField] private TextMeshProUGUI scoreText;
+
+    private void Start()
+    {
+        UpdateText();
+    }
+
+    private void Update()
     {
         float xAxis = Input.GetAxis("Horizontal");
         float yAxis = Input.GetAxis("Vertical");
@@ -14,4 +22,36 @@ public class BoatController : MonoBehaviour
         Vector3 vector3 = new Vector3(xAxis, yAxis, 0f);
         transform.position += vector3 * speed * Time.deltaTime;
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Mine"))
+        {
+            TakeDamage();
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void TakeDamage()
+    {
+        lifes--;
+        if (lifes <= 0)
+        {
+            GameOver();
+        }
+        UpdateText();
+    }
+
+    private void UpdateText()
+    {
+        hpText.text = "HP: " + lifes;
+        scoreText.text = "Очки: " + score;
+    }
+
+    private void GameOver()
+    {
+        return;
+    }
+
+
 }
